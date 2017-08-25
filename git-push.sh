3f24f786d93f72c2e34f1a0dc95000888c32af7c
abrechet <adam.brechet@gmail.com>
2017-08-25 16:13:44 +0200

    Fix circle.yml for machine executor

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,5 @@
 defaultPush='n'
-defaultMessage=$(git log | head -5 | grep "Test :")
+defaultMessage=$(git log | head -5 | grep "    ")
 git add .
 git status
 if [ "$defaultMessage" != "" ]; then
@@ -8,13 +8,13 @@ if [ "$defaultMessage" != "" ]; then
 	if [ "$message" = "$defaultMessage" ]; then
 		git commit -am "$(echo $message)"
 	else
-		git commit -am "Test :$message"
+		git commit -am "$message"
 	fi
 else
 	while [ "$message" = "" ]; do
 		read -r -p "Please, write your commit message. Default [$(echo $defaultMessage)] " message
 	done
-	git commit -am "Test :$message"
+	git commit -am "$message"
 fi
 read -p "Are you sure to push your commit ? Default : [$defaultPush] " push
 push=${push:-$defaultPush}
